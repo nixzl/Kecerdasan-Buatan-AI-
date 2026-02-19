accec19899cd83c1153e59b655d23d28781988a3
nixzl <aunizlal1@gmail.com>
2026-02-19 10:26:42 +0700

    test 2

--- Assets/Skrip/Waypoint/Route2PointSO.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-[CreateAssetMenu(menuName = "Waypoint/Route2 Point (Position)",fileName = "RoutePointPos")]
-public class Route2PointPosSO : ScriptableObject
-{
-    public Vector3 pointA;
-    public Vector3 pointB;
-}
